bf8f93d86575bf172edce3c6ebb828d247e8c367
sairampasupuleti42 <sairampasupuleti.42@gmail.com>
2018-08-29 15:14:27 +0530

    Created Category model

---
+++ application/models/Category_model.php
@@ -0,0 +1,39 @@
+<?php
+class Category_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function truncate($table)
+    {
+        $this->db->truncate($table);
+    }
+    function addCategory($pdata)
+    {
+        $this->db->set("category_created_on", "NOW()", false);
+        $this->db->set("category_status", "1", false);
+        $this->db->insert("tbl_categories", $pdata);
+        return $this->db->insert_id();
+    }
+    function updateCategory($pdata, $category_id)
+    {
+        $this->db->where("category_id", $category_id);
+        return $this->db->update("tbl_categories", $pdata);
+    }
+    function delCategory($category_id)
+    {
+        $this->db->where("category_id", $category_id);
+        return $this->db->delete("tbl_categories");
+    }
+    function getCategoryById($user_id)
+    {
+        $this->db->select("c.*");
+        $this->db->where("c.category_id", $user_id);
+        $query = $this->db->get("tbl_categories c");
+        if ($query->num_rows() > 0) {
+            return $query->row_array();
+        }
+        return false;
+    }
+}
